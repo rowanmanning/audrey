dc3f9caf72bfe5a8524bf381397638553e8c7382
Rowan Manning <accounts+github@rowanmanning.com>
2020-12-05 13:14:49 +0000

    Set cache-control headers for proxied images

--- server/controller/proxy-image.js
+++ server/controller/proxy-image.js
@@ -7,6 +7,12 @@ const userAgent = require('../lib/user-agent');
 module.exports = function mountProxyImageController(app) {
 	const {router} = app;
 
+	const maxAge = (
+		app.env === 'production' ?
+			Math.floor(app.options.publicCacheMaxAge / 1000) :
+			0
+	);
+
 	router.get('/proxy-image/:image', [
 		requireAuth(),
 		proxyImage
@@ -22,6 +28,13 @@ module.exports = function mountProxyImageController(app) {
 		// Proxy if the response is an image
 		imageRequest.on('response', httpResponse => {
 			if (isImageResponse(httpResponse)) {
+
+				// Clean up the image headers
+				httpResponse.headers = {
+					'Cache-Control': `public, max-age=${maxAge}`,
+					'Content-Length': httpResponse.headers['content-length'],
+					'Content-Type': httpResponse.headers['content-type']
+				};
 				return imageRequest.pipe(response);
 			}
 			return next(new Error('Proxy URL did not respond with an image'));
