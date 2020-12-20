d328334fe89ee307a41524cb386bf726fd7cdb76
Rowan Manning <accounts+github@rowanmanning.com>
2020-12-20 10:04:47 +0000

    Include more information in the User-Agent

--- server/lib/user-agent.js
+++ server/lib/user-agent.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const manifest = require('../../package.json');
+const gotManifest = require('got/package.json');
 
 // Get the Audrey useragent
 module.exports = function userAgent() {
-	return `Audrey/${manifest.version}`;
+	return `Audrey/${manifest.version} (+${manifest.homepage}) (Got/${gotManifest.version})`;
 };
