f7659c26e6050785c363bc892c6603449b823422
Rowan Manning <accounts+github@rowanmanning.com>
2022-03-24 23:37:38 +0000

    Fix the author info in the site footer (#143)
    This went missing when I reformatted the package.json file. I think it's
    best just to hard code this link.

--- server/view/component/section/footer.jsx
+++ server/view/component/section/footer.jsx
@@ -16,7 +16,7 @@ module.exports = class FooterSection extends Component {
 	 *     Returns a DOM element representing the website footer.
 	 */
 	render() {
-		const {author, bugs, homepage, version} = manifest;
+		const {bugs, homepage, version} = manifest;
 		const year = new Date().getFullYear();
 		return (
 			<footer role="contentinfo" class="footer page-layout">
@@ -24,7 +24,7 @@ module.exports = class FooterSection extends Component {
 					<small class="footer__copyright">
 						<a href={homepage}>Audrey</a> {` is licensed under `}
 						<a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3</a>.<br/>
-						Copyright © {year}, <a href={author.url}>{author.name}</a>.<br/>
+						Copyright © {year}, <a href="https://rowanmanning.com/">Rowan Manning</a>.<br/>
 						Powered by Audrey v{version} {' | '}
 						<a href={`${homepage}#readme`}>Documentation</a> {' | '}
 						<a href={bugs}>Report an issue</a>
