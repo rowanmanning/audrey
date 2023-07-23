05780bd30891691d76d29ecd504302355ac5481d
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2023-07-23 20:17:30 +0100

    Close the database connection on restart

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ require('dotenv').config();
 // Create and start the app
 audrey()
 	.then(app => {
+		async function handleClose() {
+			try {
+				await app.stop();
+				process.exit(0);
+			} catch (error) {
+				app.log.error({msg: `Server stop failed: ${error.message}`});
+				process.exit(1);
+			}
+		}
+		process.on('SIGINT', handleClose);
+		process.on('SIGTERM', handleClose);
+
 		return app.start(process.env.PORT || 8080);
 	})
 	.catch(error => {
